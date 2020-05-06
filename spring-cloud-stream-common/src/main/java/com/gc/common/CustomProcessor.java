83d4bd358363802176e65965664cbd7e63100fa6
gaochao <475855186@qq.com>
2020-05-06 23:58:39 +0800

    Initial commit

---
+++ spring-cloud-stream-common/src/main/java/com/gc/common/CustomProcessor.java
@@ -0,0 +1,9 @@
+package com.gc.common;
+
+/**
+ * 自定义的处理器
+ * @author gaochao
+ * @create 2020-05-06 18:14
+ */
+public interface CustomProcessor {
+}
---
+++ spring-cloud-stream-provider-mq/src/main/java/com/gc/provider/ProviderApplication.java
@@ -0,0 +1,11 @@
+package com.gc.provider;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaochao
+ * @create 2020-04-30 14:01
+ */
+@SpringBootApplication
+public class ProviderApplication {
+}
